5259790b9af917f808a995eb35c10cb7a4b2c8aa
berdikhanova <marina@minerva.kgi.edu>
2024-11-06 19:11:54 +0000

    Added code for graphs for PM2.5, NO2 and mMETs

--- presentation.R
+++ presentation.R
@@ -5,6 +5,7 @@ library(readr)
 library(ggplot2)
 library(esquisse)
 library(dplyr)
+library(stringr)
 
 ## Data baseline year for reference and scenario
 
@@ -27,6 +28,296 @@ health_intervention <- read_csv(paste0(directory,"manchester/simulationResults/F
 
 #Marina to do
 
+exposure_ref <- read_csv("manchester/simulationResults/ForUrbanTransition/reference/health/03_exposure_and_rr/pp_exposure_2025.csv")
+
+exposure_cint <- read_csv("manchester/simulationResults/ForUrbanTransition/cycleIntervention/health/03_exposure_and_rr/pp_exposure_2025.csv")
+
+dd_ref <- read_csv("manchester/simulationResults/ForUrbanTransition/reference/sp_2021_2050/dd_2025_ref.csv")
+
+dd_cint <- read_csv("manchester/simulationResults/ForUrbanTransition/cycleIntervention/dd_2025_cint.csv")
+
+zone <- read_csv("manchester/synPop/sp_2021/zoneSystem.csv")
+
+################### Data Preparation ###########################################
+
+exposure_ref <- exposure_ref %>% 
+  select(id,
+         hhid,
+         age,
+         gender,
+         mmetHr_cycle,
+         mmetHr_walk,
+         mmetHr_otherSport,
+         exposure_normalised_pm25,
+         exposure_normalised_no2) %>%
+  mutate(scenario = "Baseline") %>% 
+  left_join(dd_ref %>% 
+              select(ddid = id, hhID, zone_id = zone),
+            by = c("hhid" = "hhID")) %>% 
+  left_join(zone %>% 
+              select(location = ladnm,imd = imd10, oaID),
+            by = c("zone_id" = "oaID"))
+
+exposure_cint <- exposure_cint %>% 
+  select(id,
+         hhid,
+         age,
+         gender,
+         mmetHr_cycle,
+         mmetHr_walk,
+         mmetHr_otherSport,
+         exposure_normalised_pm25,
+         exposure_normalised_no2) %>%
+  mutate(scenario = "Cycling Intervention") %>% 
+  left_join(dd_cint %>% 
+              select(ddid = id, hhID, zone_id = zone),
+            by = c("hhid" = "hhID")) %>% 
+  left_join(zone %>% 
+              select(location = ladnm,imd = imd10, oaID),
+            by = c("zone_id" = "oaID"))
+
+exposure <- bind_rows(exposure_ref, exposure_cint)
+
+rm(exposure_ref, exposure_cint)
+
+################### Adding Zone and Demographics ########################
+
+exposure <- exposure %>% 
+  filter(age >= 18) %>% 
+  mutate(gender = factor(gender,
+                         levels = c(1,2),
+                         labels = c("Male","Female")),
+         scenario = factor(scenario, 
+                           levels = c("Baseline","Cycling Intervention")),
+         age_group = factor(case_when(
+           age >= 18 & age <= 25 ~ "18-25",
+           age >= 26 & age <= 65 ~ "26-65",
+           age >= 66 ~ "65+",
+           TRUE ~ "Other"), 
+           levels = c("18-25", "26-65", "65+", "Other")),
+         imd = factor(imd,
+                      levels = c(1,2,3,4,5,6,7,8,9,10),
+                      labels = c("Most Deprived", 2,3,4,5,6,7,8,9,"Least Deprived")),
+         total_mmetHr = mmetHr_cycle+mmetHr_walk+mmetHr_otherSport)
+
+########################## Data Visualizations #################################
+
+summary_stats_pm25 <- exposure %>%
+  group_by(scenario) %>%
+  summarize(
+    min = min(exposure_normalised_pm25),
+    IQR_low = quantile(exposure_normalised_pm25, 0.25),
+    median = median(exposure_normalised_pm25),
+    IQR_high = quantile(exposure_normalised_pm25, 0.75),
+    max = max(exposure_normalised_pm25))
+
+# Overall
+
+pm25_scenarios <- ggplot(exposure, aes(x = scenario, y = exposure_normalised_pm25, fill = scenario)) +
+  geom_boxplot() +
+  labs(title = "Weekly Individual PM2.5 Exposure Levels",
+       y = "PM2.5 (µg/m³)",
+       fill = "Scenario") +
+  theme_minimal(base_size = 16) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.title.x = element_blank(),
+    plot.title = element_text(hjust = 0.5, face = "bold"),
+    axis.text = element_text(face = "bold"),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"))
+
+# Age
+
+pm25_age <- ggplot(exposure, aes(x = age_group, y = exposure_normalised_pm25, fill = scenario)) +
+  geom_boxplot() +
+  labs(
+    title = "Weekly Individual PM2.5 Exposure Levels by Age",
+    y = "PM2.5 (µg/m³)",
+    fill = "Scenario") +
+  theme_minimal(base_size = 16) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(), 
+    axis.title.x = element_blank(),
+    plot.title = element_text(hjust = 0.5, face = "bold"), 
+    axis.text = element_text(face = "bold"),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"))
+
+# Gender
+
+pm25_gender <- ggplot(exposure, aes(x = gender, y = exposure_normalised_pm25, fill = scenario)) +
+  geom_boxplot() +
+  labs(title = "Weekly Individual PM2.5 Exposure Levels by Gender",
+       y = "PM2.5 (µg/m³)",
+       fill = "Scenario") +
+  theme_minimal(base_size = 16) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.title.x = element_blank(),
+    plot.title = element_text(hjust = 0.5, face = "bold"),
+    axis.text = element_text(face = "bold"),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"))
+
+pm25_imd <- ggplot(exposure, aes(x = imd, y = exposure_normalised_pm25, fill = scenario)) +
+  geom_boxplot() +
+  labs(
+    title = "Weekly PM2.5 Exposure Levels by Index of Multiple Deprivation (IMD)",
+    y = "PM2.5 (µg/m³)",
+    fill = "Scenario") +
+  theme_minimal(base_size = 16) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(), 
+    axis.title.x = element_blank(),
+    plot.title = element_text(hjust = 0.5, face = "bold"), 
+    axis.text = element_text(face = "bold"),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"))
+
+# No2
+
+no2_scenarios <- ggplot(exposure, aes(x = scenario, y = exposure_normalised_no2, fill = scenario)) +
+  geom_boxplot() +
+  labs(title = "Weekly Individual NO2 Exposure Levels",
+       y = "NO2 (µg/m³)",
+       fill = "Scenario") +
+  theme_minimal(base_size = 16) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.title.x = element_blank(),
+    plot.title = element_text(hjust = 0.5, face = "bold"),
+    axis.text = element_text(face = "bold"),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"))
+
+# Age
+
+no2_age <- ggplot(exposure, aes(x = age_group, y = exposure_normalised_no2, fill = scenario)) +
+  geom_boxplot() +
+  labs(
+    title = "Weekly Individual NO2 Exposure Levels by Age",
+    y = "NO2 (µg/m³)",
+    fill = "Scenario") +
+  theme_minimal(base_size = 16) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(), 
+    axis.title.x = element_blank(),
+    plot.title = element_text(hjust = 0.5, face = "bold"), 
+    axis.text = element_text(face = "bold"),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"))
+
+# Gender
+
+no2_gender <- ggplot(exposure, aes(x = gender, y = exposure_normalised_no2, fill = scenario)) +
+  geom_boxplot() +
+  labs(title = "Weekly Individual NO2 Exposure Levels by Gender",
+       y = "NO2 (µg/m³)",
+       fill = "Scenario") +
+  theme_minimal(base_size = 16) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.title.x = element_blank(),
+    plot.title = element_text(hjust = 0.5, face = "bold"),
+    axis.text = element_text(face = "bold"),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"))
+
+#IMD
+
+no2_imd <- ggplot(exposure, aes(x = imd, y = exposure_normalised_no2, fill = scenario)) +
+  geom_boxplot() +
+  labs(
+    title = "Weekly Inidividual NO2 Exposure Levels by Index of Multiple Deprivation (IMD)",
+    y = "NO2 (µg/m³)",
+    fill = "Scenario") +
+  theme_minimal(base_size = 16) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(), 
+    axis.title.x = element_blank(),
+    plot.title = element_text(hjust = 0.5, face = "bold"), 
+    axis.text = element_text(face = "bold"),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"))
+
+# mmetHR
+
+mmet_scenarios <- ggplot(exposure, aes(x = scenario, y = total_mmetHr, fill = scenario)) +
+  geom_boxplot() +
+  labs(title = "Total Marginal MET Hours per Week",
+       y = "mMET-hours/week",
+       fill = "Scenario") +
+  theme_minimal(base_size = 16) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.title.x = element_blank(),
+    plot.title = element_text(hjust = 0.5, face = "bold"),
+    axis.text = element_text(face = "bold"),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"))
+
+# Age
+
+mmet_age <- ggplot(exposure, aes(x = age_group, y = total_mmetHr, fill = scenario)) +
+  geom_boxplot() +
+  labs(
+    title = "Total Marginal MET Hours per Week by Age",
+    y = "mMET-hours/week",
+    fill = "Scenario") +
+  theme_minimal(base_size = 16) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(), 
+    axis.title.x = element_blank(),
+    plot.title = element_text(hjust = 0.5, face = "bold"), 
+    axis.text = element_text(face = "bold"),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"))
+
+# Gender
+
+mmet_gender <- ggplot(exposure, aes(x = gender, y = total_mmetHr, fill = scenario)) +
+  geom_boxplot() +
+  labs(title = "Total Marginal MET Hours per Week by Gender",
+       y = "mMET-hours/week",
+       fill = "Scenario") +
+  theme_minimal(base_size = 16) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.title.x = element_blank(),
+    plot.title = element_text(hjust = 0.5, face = "bold"),
+    axis.text = element_text(face = "bold"),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"))
+
+#IMD
+
+mmet_imd <- ggplot(exposure, aes(x = imd, y = total_mmetHr, fill = scenario)) +
+  geom_boxplot() +
+  labs(
+    title = "Total Marginal MET Hours per Week by Index of Multiple Deprivation (IMD)",
+    y = "mMET-hours/week",
+    fill = "Scenario") +
+  theme_minimal(base_size = 16) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(), 
+    axis.title.x = element_blank(),
+    plot.title = element_text(hjust = 0.5, face = "bold"), 
+    axis.text = element_text(face = "bold"),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"))
 
 
 ############################ Health ##################################################
